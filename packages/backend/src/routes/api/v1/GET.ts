d9bd3f38378a036637c5f5181d57ec139e2f2017
Trent Shailer <trent.shailer@gmail.com>
2023-01-03 19:16:00 +1300

    remove test route

--- packages/backend/src/routes/api/v1/GET.ts
+++
@@ -1,6 +0,0 @@
-import type { FastifyInstance, FastifySchema } from "fastify";
-export default async function (fastify: FastifyInstance) {
-  fastify.get("/", async (request, reply) => {
-    reply.send("pong");
-  });
-}
